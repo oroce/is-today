2e48c34851a7dccebdd30bd9e4d7ac859202b6f0
Ulrik Augustsson <ulrikaugustsson@gmail.com>
2015-08-19 11:37:06 +0200

    Convert to es6

---
+++ src/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+export default function isToday (date, {convert = false, scope = new Date()} = {}) {
+	if(typeof convert !== 'boolean') {
+		throw new TypeError('Expected convert option to be a Boolean got ' + typeof convert);
+	}
+
+	if(convert === true) {
+		date = new Date(date);
+	}
+
+	if (isNaN(Date.parse(date))) {
+		throw new TypeError('Expected first parameter to be a Date got ' + typeof date);
+	}
+
+	return date.toDateString() === scope.toDateString();
+};
